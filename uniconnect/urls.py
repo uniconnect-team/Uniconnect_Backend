fa89aba586e12fc3d94456d2f933f69881741631
mmhachem <mmh156@mail.aub.edu>
2025-11-04 18:47:45 +0200

    fixed dorm dashboard

--- uniconnect/urls.py
+++ uniconnect/urls.py
@@ -1,15 +1,16 @@
-"""uniconnect URL Configuration."""
-from __future__ import annotations
-
-from django.conf import settings
-from django.conf.urls.static import static
+# uniconnect/urls.py
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.core.urls")),
-    path("api/v1/auth/", include("apps.users.urls")),
+
+    # Same URLConf included twice, but with distinct namespaces
+    path("api/v1/auth/", include(("apps.users.urls", "users"), namespace="users_auth")),
+    path("api/users/", include(("apps.users.urls", "users"), namespace="users_api")),
 ]
 
 if settings.DEBUG:
